0f9e4552af8a1a85c755bdf29b7238d2947b7e5c
gmujica <gregorymujica1993@gmail.com>
2020-12-15 20:26:54 -0500

    changes

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
 import useStyles from './styles'
-
+import CardItem from './CartItem'
 
 //Layout
 const Cart = ({ cart }) => {
-
-    const isEmpty = !cart.line_items
 
     const classes = useStyles()
 
@@ -15,11 +13,11 @@ const Cart = ({ cart }) => {
     )
     
     const FilledCard = () => (
-            <>
+        <>
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CardItem item={item} />
                     </Grid>
                 ))}
             </Grid>
@@ -28,18 +26,20 @@ const Cart = ({ cart }) => {
                         Subtotal: {cart.subtotal.formatted_with_symbol}
                     </Typography>
                     <div>
-                        <Button className={classes.EmptyButtom} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
-                        <Button className={classes.CheckoutButtom} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
+                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                        <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
                     </div>
             </div>
         </>
     )
 
+    if(!cart.line_items) return 'Loading...'
+
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
-            { isEmpty ? <EmptyCard /> : <FilledCard /> }
+            <Typography className={classes.title} variant='h6' gutterBottom>Your Shopping Cart</Typography>
+            { !cart.line_items.length ? <EmptyCard /> : <FilledCard /> }
         </Container>
     )
 }
